80089b6d613991e4384777c0fc1f63491f6db2ae
GUS\Vladimir <atis-magnus@mail.ru>
2023-01-27 18:23:17 +0200

    Fix for HomePageTest, LivatMenuButton().click not working properly

--- src/main/java/pages/OpeningHoursPage.java
+++ src/main/java/pages/OpeningHoursPage.java
@@ -16,41 +16,42 @@ public class OpeningHoursPage extends BasePage{
     public OpeningHoursPage(WebDriver driver) {
         super(driver);
     }
-    public static final String OpeningHoursUrl = "https://www.livat.com/en/hammersmith/opening-hours";
-    public static final String IKEAshopUrl = "https://www.livat.com/en/hammersmith/shops/ikea";
-    public static final String OpeningHoursTitle = "Opening Hours | Livat";
+    public static final String OpeningHoursUrl = "https://www.livat.com/hammersmith/en/opening-hours";
+    public static final String IKEAshopUrl = "https://www.livat.com/hammersmith/en/shops/ikea";
+    public static final String OpeningHoursTitle = "Opening hours | Livat - Make Today Different";
 
     public WebElement FindShopInput(){
-        return driver.findElement(By.id("form-search"));
+        return driver.findElement(By.id("search"));
     }
-    public WebElement FindShopAutoOptions(){ return driver.findElement(By.id("stores-list"));}
+    public WebElement FindShopAutoOptions(){ return driver.findElement(By.id("tenantList"));}
+    public WebElement QuickSearchResultLink(){ return driver.findElement(By.className("underline"));}
+
     public List<WebElement>optionsToSelect() {return FindShopAutoOptions().findElements(By.tagName("option"));}
 
-    public WebElement QuickSearchResultLink(){ return driver.findElement(By.id("opening-hour-quick-search--result-link"));}
+
 
     public void verifyFindShop_isEmpty(){
         Assert.assertEquals("", FindShopInput().getAttribute("value"), "Field should be empty");
     }
 
-    public void verifyFindShopOutline_whenNOTClicked() {  //check for outline color when NOT clicked
-        FindShopInput().clear();
-
-        String outlineNotSelected = FindShopInput().getCssValue("outline-color"); //rgba(118, 117, 113, 1) - серый, поле не выбрано
-        System.out.println(outlineNotSelected); //for debugging
-        Assert.assertEquals(outlineNotSelected, "rgba(118, 117, 113, 1)", "Wrong outline color when FindShop NOT selected" );
-    }
-    public void verifyFindShopOutline_whenClicked()  {  //check for outline color when clicked
-
-        FindShopInput().clear();
-
-        String outlineSelected = FindShopInput().getCssValue("outline-color"); //rgba(213, 205, 193, 1) - коричневый, внешний оутлайн при выборе поля
-        System.out.println(outlineSelected);//for debugging
-        Assert.assertEquals(outlineSelected, "rgba(213, 205, 193, 1)", "Wrong outline color when FindShop IS selected" );
-
-        String FindShopText_default = "Find shop";
-        System.out.println(FindShopText_default);
-        Assert.assertEquals(FindShopInput().getAttribute("placeholder"), FindShopText_default, "Wrong default text for blank input");
-    }
+//    public void verifyFindShopOutline_whenNOTClicked() {  //check for outline color when NOT clicked - поменяли цвета - теперь не нужно
+//        FindShopInput().clear();
+//        String outlineNotSelected = FindShopInput().getCssValue("border-color"); //rgba(118, 117, 113, 1) - серый, поле не выбрано
+//        System.out.println(outlineNotSelected); //for debugging
+//        Assert.assertEquals(outlineNotSelected, "rgba(118, 117, 113, 1)", "Wrong outline color when FindShop NOT selected" );
+//    }
+//    public void verifyFindShopOutline_whenClicked()  {  //check for outline color when clicked - - поменяли цвета - теперь не нужно
+//
+//        FindShopInput().clear();
+//
+//        String outlineSelected = FindShopInput().getCssValue("border-color"); //rgba(213, 205, 193, 1) - коричневый, внешний оутлайн при выборе поля
+//        System.out.println(outlineSelected);//for debugging
+//        Assert.assertEquals(outlineSelected, "rgba(213, 205, 193, 1)", "Wrong outline color when FindShop IS selected" );
+//
+//        String FindShopText_default = "Find shop";
+//        System.out.println(FindShopText_default);
+//        Assert.assertEquals(FindShopInput().getAttribute("placeholder"), FindShopText_default, "Wrong default text for blank input");
+//    }
 
     public void verifyFindShop_whenTypedSomething(String textInput){
         FindShopInput().clear();
@@ -91,9 +92,9 @@ public class OpeningHoursPage extends BasePage{
     }
 
     public void verifyIKEAinfo_IsDisplayed(){
-        WebElement QuickSearchResultHeading = driver.findElement(By.id("opening-hour-quick-search--results-heading"));
+        WebElement QuickSearchResultHeading = driver.findElement(By.xpath("//*[@id='gatsby-focus-wrapper']/div/main/div/div[2]/div[2]/div/div[2]"));
         Assert.assertEquals(QuickSearchResultHeading.getAttribute("textContent"), "IKEA", "Fail: wrong header in results of quick search");
-        Assert.assertEquals(QuickSearchResultLink().getAttribute("href"), "https://www.livat.com/en/hammersmith/shops/ikea", "Fail: wrong link in results of quick search ");
+        Assert.assertEquals(QuickSearchResultLink().getAttribute("href"), "https://www.livat.com/hammersmith/en/shops/ikea", "Fail: wrong link in results of quick search ");
 
     }
 
